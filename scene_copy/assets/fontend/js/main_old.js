328143652638ee5790c63ddd65a6fa078cfe92ba
Sayantan Bhakta <sayantan.ece.imps@gmail.com>
2026-04-22 22:40:22 +0530

    delete duplicate folders

--- scene_copy/assets/fontend/js/main_old.js
+++
@@ -1,1 +0,0 @@
-//import o from"./drupal-off-canvas.js";import r from"./hero-ambient-popup-video.js";import a from"./hero-banner-popup-video.js";import m from"./map-of-california-custom-themed.js";o(),r(),a(),m();
--- scene_copy/assets/fontend/js/primary-nav.nuKHgqPrYVsT.js
+++
@@ -1,1 +0,0 @@
-import r from"./primary-nav.EIN3KI_nzq1M.js";r();
